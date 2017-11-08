4682693c10a6419dc09dadd7a293d55a93b0966b
Alexey S. Kiselev <alexeys.kiselev@yahoo.com>
2017-11-08 12:25:09 +0300

    2.0.4 - Add Normal (Gaussian) distribution

--- core/methods/normal.js
+++ core/methods/normal.js
@@ -1,38 +1,195 @@
 // @flow
 /**
  * Normal Gaussian Distribution
+ * https://en.wikipedia.org/wiki/Normal_distribution
  * @param mu: number - Mu value of Normal Distribution
  * @param sigma: number - Sigma value of Normal Distribution
  * @returns Normal Distributed value based on parameters
  * Created by Alexey S. Kiselev
  */
 
+let Uniform = require('./uniform');
+
 class Normal {
     constructor(mu: number, sigma: number): void {
         this.mu = mu;
         this.sigma = sigma;
+        /**
+         * Create an instance of Uniform class
+         * Use this class for generation random normal distributed numbers
+         * @type {Uniform}
+         */
+        this.uniform = new Uniform(-1, 1);
     }
 
+    /**
+     * Generates a random number
+     * Uses Box-Muller transform and Marsaglia polar method
+     * This method generats two random numbers, but use one of it
+     * @returns a normal distributed number
+     */
     random(): number {
-        return this.mu;
+        let U1: number,
+            U2: number,
+            W: number,
+            mult: number;
+        do {
+            U1 = this.uniform.random();
+            U2 = this.uniform.random();
+            W = Math.pow(U1, 2) + Math.pow(U2, 2);
+        } while(W >= 1 || W === 0);
+        mult = Math.sqrt((-2*Math.log(W))/W);
+
+        return this.mu + this.sigma * U1 * mult;
     }
 
+    /**
+     * Generates normal distributed numbers
+     * Uses the same algorithm as in .random() method
+     * Add both two random generated values to array
+     * For this purpose use algorithm again (not .random() method)
+     * @param n: number - Number of elements in resulting array, n > 0
+     * @returns Array<number> - normal distributed numbers
+     */
     distribution(n: number): Array<number> {
-        return [this.sigma];
+        if(n <= 1){
+            n = 1;
+        }
+        let normalArray: Array<number> = [],
+            U1: number,
+            U2: number,
+            W: number,
+            mult: number;
+        for(let i: number = 0; i < n; i += 2){
+            do {
+                U1 = this.uniform.random();
+                U2 = this.uniform.random();
+                W = Math.pow(U1, 2) + Math.pow(U2, 2);
+            } while(W >= 1 || W === 0);
+            mult = Math.sqrt((-2*Math.log(W))/W);
+            normalArray[i] = this.mu + this.sigma * U1 * mult;
+            normalArray[i + 1] = this.mu + this.sigma * U2 * mult;
+        }
+        /**
+         * This algorithm generates two values
+         * So, if we need odd-length array check value "n"
+         * if "n" is odd - delete last element
+         */
+        if(n % 2 !== 0){
+            normalArray.pop();
+        }
+        return normalArray;
     }
 
+    /**
+     * Error handling
+     * So, there is no situation, when an error can occurs
+     * In this case method simply returns "false"
+     * @returns {boolean}
+     */
     isError(): boolean | {error: string} {
         return false;
     }
 
-    refresh(newMu: number, newSigma: number): Normal {
+    /**
+     * Refresh method
+     * @param newMu: number - new parameter "mu"
+     * @param newSigma: number - new parameter "sigma"
+     * This method does not return values
+     */
+    refresh(newMu: number, newSigma: number): void {
         this.mu = newMu;
         this.sigma = newSigma;
-        return this;
     }
 
+    /**
+     * Class .toString method
+     * @returns {string}
+     */
     toString(): string {
-        return 'Normal distribution';
+        let info = [
+            'Normal (Gaussian) Distribution',
+            `Usage: randomjs.normal(${this.mu}, ${this.sigma}).random()`
+        ];
+        return info.join('\n');
+    }
+
+    /**
+     * Mean value
+     * Information only
+     * For calculating real mean value use analyzer
+     */
+    get mean() {
+        return this.mu;
+    }
+
+    /**
+     * Median value
+     * Information only
+     * For calculating real median value use analyzer
+     */
+    get median() {
+        return this.mu;
+    }
+
+    /**
+     * Mode value - value, which appears most offen
+     * Information only
+     * For calculating real mode value use analyzer
+     */
+    get mode() {
+        return this.mu;
+    }
+
+    /**
+     * Variance value
+     * Information only
+     * For calculating real variance value use analyzer
+     */
+    get variance() {
+        return Math.pow(this.sigma, 2);
+    }
+
+    /**
+     * Skewness value
+     * Information only
+     * For calculating real skewness value use analyzer
+     */
+    get skewness() {
+        return 0;
+    }
+
+    /**
+     * Entropy value
+     * Information only
+     * For calculating real entropy value use analyzer
+     */
+    get entropy() {
+        return Math.log(2* Math.PI * Math.E * this.variance) / 2;
+    }
+
+    /**
+     * Fisher information matrix
+     * Information only
+     */
+    get fisher() {
+        return [[(1 / this.variance), 0], [0, (0.5 / Math.pow(this.variance, 2))]];
+    }
+
+    /**
+     * All parameters of distribution in one object
+     * Information only
+     */
+    get parameters() {
+        return {
+            mean: this.mean,
+            median: this.median,
+            mode: this.mode,
+            variance: this.variance,
+            skewness: this.skewness,
+            entropy: this.entropy,
+            fisher: this.fisher
+        };
     }
 }
 
